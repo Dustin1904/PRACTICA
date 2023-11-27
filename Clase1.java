c46f302a1c565be6bffe311ead192858450576fd
JohnMata0427 <john.mata@epn.edu.ec>
2023-11-27 12:44:27 -0500

    nuevos cambios

--- Clase1.java
+++ Clase1.java
@@ -1,3 +1,55 @@
+import java.util.Scanner;
 public class Clase1 {
-    
+    //atributos
+    public String nombre;
+    public String raza;
+    public int edad;
+
+    //constructor
+
+    public Clase1(String nombre) {
+        this.nombre = nombre;
+    }
+
+    //metodo getters y setters
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getRaza() {
+        return raza;
+    }
+
+    public void setRaza(String raza) {
+        this.raza = raza;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    //metodo entrada de datos
+    public void entradaDatos(){
+        Scanner entrada= new Scanner(System.in);
+        System.out.println("Ingrese la raza: ");
+        setRaza(entrada.nextLine());
+
+        System.out.println("Ingrese el edad: ");
+        setEdad(entrada.nextLine());
+
+    }
+
+    public void salidaDatos(){
+        System.out.println("raza: " + getRaza());
+        System.out.println("edad: " + getEdad());
+    }
 }
--- Main.java
+++ Main.java
@@ -1,7 +1,18 @@
 public class Main {
     public static void main(String[] args){
-        System.out.println("Hola xd");
-        System.out.println("hola");
+        //objeto
+        Clase1 perro1;
+        Clase1 perro2;
 
+        perro1 = new Clase1("Pepito");
+        perro2 = new Clase1("Carlitos");
+
+        //objeto y metodo
+        perro1.entradaDatos();
+        perro2.entradaDatos();
+        System.out.println("Perro: "+ perro1.getNombre());
+        perro1.salidaDatos();
+        System.out.println("perro: " + perro2.getNombre());
+        perro2.salidaDatos();
     }
 }
